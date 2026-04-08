7b65d82fd9793965667dd0b4d0abf1c7af254e86
Yeremy Acuña <182255189+Yeremy17@users.noreply.github.com>
2026-04-08 16:57:56 -0500

    Fix: Ready project for work

--- PlaNetProject/PlaNetForm.cpp
+++ PlaNetProject/PlaNetForm.cpp
@@ -10,10 +10,3 @@ void main()
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::Run(gcnew PlaNetProject::PlaNetForm()); // gcnew "NombreDelProyecto":"NombreDelForms"()
 }
-
-// Model: Clases: .cpp, .h
-// Resource: Imagenes, Fondos, SpritesImagen
-// Service: Logica de programa: Service.cpp, Service.h
-// UI: Formularios: Frm.cpp, Frm.h
-
-// Source.cpp: Main principal
