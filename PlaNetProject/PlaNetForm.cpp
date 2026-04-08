8f18878016fe730a416e18030f55b31ba71f78b9
Yeremy Acuña <261862319+yeremyacuna@users.noreply.github.com>
2026-04-08 13:45:46 -0500

    Add: PlaNetForm.h to PlaNetProject

--- PlaNetProject/PlaNetForm.cpp
+++ PlaNetProject/PlaNetForm.cpp
@@ -1,2 +1,19 @@
 #include "PlaNetForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+
+void main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew PlaNetProject::PlaNetForm()); // gcnew "NombreDelProyecto":"NombreDelForms"()
+}
+
+// Model: Clases: .cpp, .h
+// Resource: Imagenes, Fondos, SpritesImagen
+// Service: Logica de programa: Service.cpp, Service.h
+// UI: Formularios: Frm.cpp, Frm.h
+
+// Source.cpp: Main principal
