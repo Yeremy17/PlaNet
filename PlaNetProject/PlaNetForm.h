cd724ac4175757d9e7b4e52b18545157c197adfd
Yeremy Acuña <261862319+yeremyacuna@users.noreply.github.com>
2026-04-08 13:53:40 -0500

    Fix: Add .gitignore

--- PlaNetProject/PlaNetForm.h
+++ PlaNetProject/PlaNetForm.h
@@ -48,11 +48,17 @@ namespace PlaNetProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"PlaNetForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// PlaNetForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(338, 302);
+			this->Name = L"PlaNetForm";
+			this->Text = L"PlaNetForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
